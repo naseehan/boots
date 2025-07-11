5994c6b7ac05ebe56aad54640541219b6d502705
Naseeh AN <naseehan700@gmail.com>
2025-07-11 14:52:22 +0530

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import HomeNavbar from "./components/HomeNavbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Products from "./pages/Products";
@@ -9,12 +9,18 @@ import ProductDetails from "./pages/ProductDetails";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
 
+const location = useLocation()
+const isHome = location.pathname === '/'
   // going back to the specific container in the previous webpage if clicked on a button
   // and goes to another page
+
 useEffect(() => {
+
+
 if ('scrollRestoration' in window.history) {
   window.history.scrollRestoration = 'auto'; // 
 }
@@ -23,7 +29,7 @@ if ('scrollRestoration' in window.history) {
 
   return (
     <div>
-      <Navbar />
+      {isHome ? <HomeNavbar /> : <Navbar />}
 
       <Routes>
         <Route path="/" element={<Home />} />
